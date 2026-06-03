7a502e6498cf7474fd897e87c43362ec46b4d4df
whatthegrad <whatthegrad.in@gmail.com>
2026-06-03 16:49:39 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,17 +3,21 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { CartProvider } from '@/components/CartContext';
 import CustomCursor from '@/components/CustomCursor';
-
+import Script from 'next/script'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        <CartProvider>
-          <IntroAnimation />
-          <CustomCursor />
-          {children}
-        </CartProvider>
-      </body>
-    </html>
+  <html lang="en">
+        <head>
+          <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet"/>
+        </head>
+        <body>
+          <CartProvider>
+            <IntroAnimation />
+            <CustomCursor />
+            {children}
+          </CartProvider>
+          <Script src="https://assets.calendly.com/assets/external/widget.js" strategy="lazyOnload"/>
+        </body>
+      </html>
   );
 }
